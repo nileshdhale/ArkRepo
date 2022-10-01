859ec3a3202bba0162c3deb263a6d7fff176b5cf
nileshdhale <nilesh.rkr@gmail.com>
2022-10-01 16:49:04 +0530

    test cases added amruta

--- New/src/test/java/com/tests/TestP.java
+++ New/src/test/java/com/tests/TestP.java
@@ -4,8 +4,14 @@ import org.testng.annotations.Test;
 
 public class TestP {
 	@Test
-	public void TestCase2() {
+	public void AllTests() {
+		System.out.println("Test case 1 passed");
 		System.out.println("Test case 2 passed");
+		System.out.println("Test case 3 passed");
+		System.out.println("Test case 4 passed");
+		System.out.println("Test case 5 passed");
+		System.out.println("Amruta");
+		
 		
 	}
 
